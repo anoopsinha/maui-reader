36b78fc5c9bf7e70d171b8c9716dc64e5dde8282
anoopsinha <anoop.k.sinha@gmail.com>
2012-02-12 15:28:22 -0800

    trying data stuff

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,10 @@
 function getConfig() {
   return {data:config};
 }
+
+
+exports.getConfig = function(params, callback) {
+    console.log('in getConfig with ts:' + Date.now());
+    var cfg = require("config.js");
+    return callback(null, {data: cfg.config});
+};
