eb15b294ec01d92e452cfe96d0725f1618173afe
anoopsinha <anoop.k.sinha@gmail.com>
2012-02-12 16:48:27 -0800

    trying act

--- cloud/main.js
+++ cloud/main.js
@@ -6,6 +6,13 @@ function helloWorld() {
     return "hi mom";
 }
 
+
+exports.helloWorld = function(params, callback) {
+    console.log('in getConfig with ts:' + Date.now());
+    return callback(null, {data: "hi mom"});
+};
+
+
 exports.getConfig = function(params, callback) {
     console.log('in getConfig with ts:' + Date.now());
     var cfg = require("config.js");
